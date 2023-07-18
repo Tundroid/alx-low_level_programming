299d957513d230eb59bc209966ccbee72735a716
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-07-18 23:51:16 +0100

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _abs - Computes the absolute of an integer
+* @n: n, number to be computed
+*
+* Return: asolute of n
+*/
+int _abs(int n)
+{
+	return (n < 0 ? n * -1 : n);
+}
