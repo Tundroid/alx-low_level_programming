eeefc08a54d56bf84bcffc79c3e0c7d7bb177fae
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-07-19 13:14:39 +0100

    Task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - check the code.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	int sum = 3;
+	int i = 4;
+
+	while (i < 10)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+		i++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
