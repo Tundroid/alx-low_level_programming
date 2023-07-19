dda88c93bf5861ec99354fba397ac9c12d298e80
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-07-19 01:39:03 +0100

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* add - Adds two integers
+* @a: first integer to be added
+* @b: second integer to be added to a
+*
+* Return: sum of a and b
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
