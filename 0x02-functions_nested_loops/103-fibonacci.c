0367c665a7005e363c1d746192d52a0c007e6c85
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-07-19 15:06:24 +0100

    Task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdint.h>
+
+/**
+* main - check the code.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	int n = 2, n_prev = 1, tmp, sum = 0;
+
+	while (n <= 20)
+	{
+		sum += n % 2 == 0 ? n : 0;
+		tmp = n_prev;
+		n_prev = n;
+		n += tmp;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
