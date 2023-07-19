6c4430ed1e644e3b4f782c51156af24da29a3399
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-07-19 11:30:48 +0100

    Task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+* print_times_table - Print 0xn times table
+* @n: times table factor
+*/
+void print_times_table(int n)
+{
+	int i, j, product, k;
+	int arr[] = {' ', ' ', ' ', '0'};
+
+	if (n < 0 || n > 15)
+		return;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			product = i * j;
+
+			for (k = 3; k >= 0; k--)
+			{
+				if (product > 0)
+				{
+					arr[k] = 48 + product % 10;
+					product /= 10;
+				}
+				else
+				{
+					break;
+				}
+			}
+
+			for (k = 0; k < 4; k++)
+				_putchar(arr[k]);
+
+			if (j != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
