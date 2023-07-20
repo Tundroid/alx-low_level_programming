77063e7efecaa3532b99b706e6788d758534a3b3
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-07-21 00:39:00 +0100

    Fixed task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,24 +4,23 @@
 */
 void more_numbers(void)
 {
-	int i, j, k;
+	int i, j, k, l;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0;  j < 15; j++)
 		{
-			int tmp = j;
 
-			for (k = 0; k < (j > 9) ? 2 : 1; k++)
+			for (k = 0; k < ((j > 9) ? 2 : 1); k++)
 			{
+				l = j;
 				if (k == 0 && j > 9)
-					tmp /= 10;
+					l /= 10;
 				else
-					tmp %= 10;
-				_putchar(tmp + 48);
+					l %= 10;
+				_putchar(l + 48);
 			}
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
