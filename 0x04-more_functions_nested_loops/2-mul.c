2521e9e827afff231d273d1aee8067b7bd45e620
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-07-20 22:55:24 +0100

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* _mu; - multiplies 2 integers
+* @a: First integer to be multiplied
+* @b: Second integer to be multiplied
+*
+* Return: product of a and b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
