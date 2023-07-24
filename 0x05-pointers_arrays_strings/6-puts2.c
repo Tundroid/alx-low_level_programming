c77c7a5d18768caddbcf0ac05bbd79cb53af92b0
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-07-24 13:24:50 +0100

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* puts2 - prints everyother character of a string to the stdout
+* @str: string to be printed
+*/
+void puts2(char *str)
+{
+	int i = 0, j = 0;
+
+	while (*(str + i++) != '\0')
+		;
+	while (j < i)
+		_putchar(*(str + (i++)++));
+	_putchar('\n');
+}
