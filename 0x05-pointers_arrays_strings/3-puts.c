33bb264d885673488ddb3c1863935184be678baa
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-07-24 12:37:43 +0100

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _puts - prints a string to the stdout
+* @str: string to be printed
+*/
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+		_putchar(*(s + i++));
+	_putchar('\n');
+}
