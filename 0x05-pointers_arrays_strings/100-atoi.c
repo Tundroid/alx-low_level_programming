ca976ed857cb0a92363357237ee2fe76181cb2ba
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-07-24 16:27:45 +0100

    Task 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdint.h>
+/**
+* _atoi - converts string to integer
+* @s: string of characters
+*
+* Return: number converted from string or 0
+*/
+int _atoi(char *s)
+{
+	int i = 0, sign = 1, number = 0;
+
+	while (s[i] != '\0')
+	{
+		if (number == 0 && s[i] == '-')
+		{
+			sign *= -1;
+		}
+		else
+		{
+			if (isdigit(s[i]))
+			{
+				number = 10 * number + s[i] - 48;
+			}
+			else if (number != 0)
+			{
+				break;
+			}
+		}
+		i++;
+	}
+	return (number * sign);
+}
