9b3ea80a152eed49c59cbe6812a13d53bfb1cf4a
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-07-24 12:31:09 +0100

    Fixed task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-* s_strlen - counts the length of a string
+* _strlen - counts the length of a string
 * @s: string whose length is to be determined
 *
 * Return: length of s
 */
-int s_strlen(char *s)
+int _strlen(char *s)
 {
-	int length = 0, i = 0;
+	int i = 0;
 
 	while (*(s + i++) != '\0')
 		;
