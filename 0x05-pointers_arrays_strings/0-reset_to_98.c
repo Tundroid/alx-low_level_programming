b1ce06d7596a0b433ef76c466542f9d466995ad8
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-07-24 11:33:45 +0100

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+* reset_to_98 - Update the value of a pointer
+* @n: pointer to an integer to be updated
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef MOLeCULE_PUTCHAR
+#define MOLeCULE_PUTCHAR
+
+int _putchar(char);
+void reset_to_98(int *n;
+)
+#endif
