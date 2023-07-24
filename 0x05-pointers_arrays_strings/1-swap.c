c9d46f3f01faac9684ae48526be795dd7363eff6
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-07-24 11:49:18 +0100

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* swap_int - Swap two integers using pointers
+* @a: pointer to fist integer to be swapped
+* @b: pointer to second integer to be swapped
+*/
+void swap_int(int *a, int *b)
+{
+	*a += *b;
+	*b -= *a;
+	*a -= *b;
+}
