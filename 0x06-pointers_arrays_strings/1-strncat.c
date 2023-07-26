e697c8098efa9c88a9935d536f9ad76aa52d4707
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-07-26 20:46:42 +0100

    Task 1 - Fixed betty style

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,14 +3,14 @@
 #include <stdlib.h>
 /**
 * _strncat - appends one string unto another another
-* @dest: string to be appended
-* @src: string to be copied
+* @d: string to be appended
+* @s: string to be copied
 * @n: number of bytes to copy from src
 *
 * Return: pointer to destination buffer
 */
-char *_strncat(char *dest, char *src, int n)
+char *_strncat(char *d, char *s, int n)
 {
-	strncpy(dest + strlen(dest), src, (size_t)n > strlen(src) ? strlen(src) : (size_t)n);
-	return (dest);
+	strncpy(d + strlen(d), s, (size_t)n > strlen(s) ? strlen(s) : (size_t)n);
+	return (d);
 }
