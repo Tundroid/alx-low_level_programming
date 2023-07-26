d2866c5e0478dc23c25f49bdccdf5eaf90e5bbf4
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-07-26 21:04:41 +0100

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _strncpy - copies n bytes of one string unto another another
+* @dest: destination of copied string
+* @src: source of copied string
+* @n: number of bytes to be copied
+*
+* Return: pointer to destination buffer
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (1)
+	{
+		dest[i] = src[i];
+		if (i == n - 1 || src[i++] == '\0')
+			break;
+	}
+	return (dest);
+}
