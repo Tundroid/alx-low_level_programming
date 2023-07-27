00c1e650b34c6617a6c84c55bca3bc82d5d3f917
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-07-27 17:38:46 +0100

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_touppoer.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+/**
+* string_toupper - changes all lowercase letters to uppercase
+* @s: string to be changed to uppercase
+*
+* Return: pointer to string s buffer
+*/
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 97 && s[i] <= 97 + 25)
+			s[i] = s[i] - 32;
+		i++;
+	}
+	return (s);
+}
