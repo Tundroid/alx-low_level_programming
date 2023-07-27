1ee9c4f68299695a3e33593be796964ce307135b
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-07-27 16:57:15 +0100

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,17 +6,32 @@
 *
 * Return: 0 is equat, -ve if s1 < s2 and +ve if s1 > s2
 */
-char *_strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
-	int i = 0, n;
-	n = strlen(s1) > strlen(s2) ? strlen(s1) : strlen(s2);
+	int i = 0;
 
 	while (1)
 	{
-		if (s1[i] != '\0' 
-		dest[i] = src[i];
-		if (i == n - 1 || src[i++] == '\0')
+		if (s1[i] != '\0' && s2[i] != '\0')
+		{
+			if (s1[i] < s2[i])
+				return (-1);
+			else if (s1[i] > s2[i])
+				return (1);
+		}
+		else if (s1[i] == '\0' && s2[i] == '\0')
+		{
 			break;
+		}
+		else if (s1[i] == '\0')
+		{
+			return (-1);
+		}
+		else if (s2[i] == '\0')
+		{
+			return (1);
+		}
+		i++;
 	}
-	return (dest);
+	return (0);
 }
