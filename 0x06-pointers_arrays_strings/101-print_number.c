af7cb4705c94d78707c987867677e22fdf00f268
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-07-30 12:11:18 +0100

    Fixed task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,63 +1,24 @@
 #include "main.h"
-#include <stdlib.h>
 
-int _pow(int, int);
 /**
-* print_number - prints an integer to the stdout
-* @n: integer to be printed
-*/
+ * print_number - prints an integer;
+ * @n: integer to be printed;
+ */
 void print_number(int n)
 {
-	unsigned int x, m;
-	int radix_power = 0;
+	unsigned int n1;
 
-	if (n == 0)
-	{
-		_putchar(n + 48);
-		return;
-	}
 	if (n < 0)
 	{
+		n1 = -n;
 		_putchar('-');
-		n = -n;
-	}
-	m = n;
-	while (m >= 10)
+	} else
 	{
-		m =  m / 10;
-		radix_power++;
+		n1 = n;
 	}
-	/*
-	* Using log below works, but for the
-	* compilation flags used for the task
-	* it cannot work with using -lm flag
-	*/
-	/* radix_power = log((double)abs(n)); */
 
-	while (radix_power >= 0)
-	{
-		x = n / _pow(10, radix_power--);
-		_putchar(x % 10 + 48);
-	}
-}
+	if (n1 / 10)
+		print_number(n1 / 10);
 
-
-/**
-* _pow - raises an integer to the power of another
-* @b: base
-* @p: power
-*
-* Return: b^p
-*/
-int _pow(int b, int p)
-{
-	if (p == 0)
-		return (1);
-	else if (p == 1)
-		return (b);
-
-	while (p > 0)
-		return (b * _pow(b, --p));
-
-	return (-1);
+	_putchar((n1 % 10) + '0');
 }
