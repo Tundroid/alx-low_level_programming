cdaded3c30e9cdcb36479ba42525c9175c410d4d
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-01 07:18:32 +0100

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+/**
+* _strpbrk - finds the first of occurence in a
+* string of any bytes in another string
+* @s: string being searched
+* @accept: string whose individual bytes are being looked for
+*
+* Return: address of first occurence in s or NULL is none is found
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0, j;
+
+	while (accept[i] != '\0')
+	{
+		j = 0;
+		while (s[j] != '\0')
+		{
+			if (s[j] == accept[i])
+				return (s + j);
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
