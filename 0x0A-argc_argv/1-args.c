36ffed4118419b780cddb8b2c23142838ff75e40
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-04 22:36:17 +0100

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - program to print the number of command
+* line args
+* @argc: count of command line args
+* @argv: pointer to array of strings of all
+* command line args
+*
+* Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
