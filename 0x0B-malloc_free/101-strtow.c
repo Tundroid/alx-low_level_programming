587a10b57fddf55cb4b96f66ad1200ecac7a84eb
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-08 23:50:11 +0100

    Task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -22,7 +22,7 @@ char **strtow(char *str)
 		if (str[i == 0 ? i : i - 1] != ' ' && (str[i] == ' ' || str[i] == '\0'))
 			wc++;
 	}
-	words = (char **)malloc(wc > 0 ? ++wc : wc * sizeof(char *));
+	words = (char **)malloc((wc > 0 ? ++wc : wc) * sizeof(char *));
 	if (words != NULL)
 	{
 		for (i = 0; i <= (int)strlen(str); i++)
