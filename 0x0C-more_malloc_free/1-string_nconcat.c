76e194af82e5239cb331e58302b3e8fcd56d6fe3
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-10 15:14:24 +0100

    Task 1 - 2

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,62 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+/**
+* string_nconcat - concatenates two strings using
+* n bytes from the second string
+* @s1: string 1
+* @s2: string 2
+* @n: number of bytes from s2 to be used
+*
+* Return: pointer to concatinated string or NULL on failure
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *tmp;
+	int alloced_s2 = 0;
+
+	if (s1 == NULL)
+	{
+		s1 = malloc(sizeof(char));
+		if (s1 == NULL)
+			return (NULL);
+		s1 = '\0';
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = malloc(sizeof(char));
+		if (s2 == NULL)
+		{
+			free(s1);
+			return (NULL);
+		}
+		s2 = '\0';
+		alloced_s2 = 1;
+	}
+
+	tmp = malloc(strlen(s1) + 1);
+	if (tmp == NULL)
+	{
+		free(s1);
+		free(s2);
+		return (NULL);
+	}
+
+	strcpy(tmp, s1);
+	s1 = malloc(strlen(s1) + n + 1);
+	if (s1 == NULL)
+	{
+		free(s1);
+		free(s2);
+		free(tmp);
+		return (NULL);
+	}
+	strcpy(s1, tmp);
+	strncat(s1, s2, n);
+	if (alloced_s2)
+		free(s2);
+	free(tmp);
+	return (s1);
+}
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+* _calloc - dynamically allocates memory for an array
+* @nmemb: size of array
+* @size: size of each array element
+*
+* Return: pointer to allocated memory location
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	arr = malloc(nmemb * size);
+	return (arr);
+}
