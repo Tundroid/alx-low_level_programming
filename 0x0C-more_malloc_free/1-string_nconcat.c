c5d270a3b45f03d932e30d717152327fba13dd1d
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-11 22:43:39 +0100

    Trying task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,31 +1,34 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
-char *string_nconcat(char *s1, char *s2, unsigned int n);
+
 /**
-* string_nconcat - concatenates two strings using
-* n bytes from the second string
-* @s1: string 1
-* @s2: string 2
-* @n: number of bytes from s2 to be used
-*
-* Return: pointer to concatinated string or NULL on failure
+* string_nconcat - function
+* @s1: kdk
+* @s2: dkd
+* @n: int
+* Return: dkd
 */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *str;
+	char *new_str;
+	unsigned int i, j;
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
-	n = n >= strlen(s2) ? strlen(s2) : n;
-	str = malloc(strlen(s1) + n + 1);
-	if (str != NULL)
-	{
-		strcpy(str, s1);
-		strncat(s1, s2, n);
-	}
-	return (str);
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	new_str = malloc(sizeof(char) * (i + n + 1));
+	if (new_str == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		new_str[i] = s1[i];
+	for (j = 0; s2[j] != '\0' && j < n; j++)
+		new_str[i + j] = s2[j];
+	new_str[i + j] = '\0';
+	return (new_str);
 }
+
