e244ad398b77d0781087cf41b440072f3713f6b4
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-11 22:33:10 +0100

    Fixed task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,53 +13,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *tmp;
-	int alloced_s1 = 0, alloced_s2 = 0;
+	char *str;
 
 	if (s1 == NULL)
-	{
-		s1 = malloc(sizeof(char));
-		if (s1 == NULL)
-			return (NULL);
-		*s1 = '\0';
-		alloced_s1 = 1;
-	}
+		s1 = "";
 
 	if (s2 == NULL)
+		s2 = "";
+	n = n >= strlen(s2) ? strlen(s2) : n;
+	str = malloc(strlen(s1) + n + 1);
+	if (str != NULL)
 	{
-		s2 = malloc(sizeof(char));
-		if (s2 == NULL)
-		{
-			free(s1);
-			return (NULL);
-		}
-		*s2 = '\0';
-		alloced_s2 = 1;
+		strcpy(str, s1);
+		strncat(s1, s2, n);
 	}
-
-	tmp = malloc(strlen(s1) + 1);
-	if (tmp == NULL)
-	{
-		free(s1);
-		free(s2);
-		return (NULL);
-	}
-
-	strcpy(tmp, s1);
-	s1 = malloc(strlen(s1) + (n >= strlen(s2) ? strlen(s2) : n) + 1);
-	if (s1 == NULL)
-	{
-		if (alloced_s1)
-			free(s1);
-		if (alloced_s2)
-			free(s2);
-		free(tmp);
-		return (NULL);
-	}
-	strcpy(s1, tmp);
-	strncat(s1, s2, n);
-	if (alloced_s2)
-		free(s2);
-	free(tmp);
-	return (s1);
+	return (str);
 }
