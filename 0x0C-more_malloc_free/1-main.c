c0f5d96aa2cfbf53409db4a2d4b034f0add49998
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-11 14:33:47 +0100

    Fixed task 1

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -11,6 +11,10 @@ int main(void)
 {
     char *concat;
 
+    concat = string_nconcat("Hello", NULL, 12);
+    printf("%s\n", concat);
+    concat = string_nconcat(NULL, "Hello", 0);
+    printf("%s\n", concat);
     concat = string_nconcat(NULL, NULL, 10);
     printf("%s\n", concat);
     free(concat);
