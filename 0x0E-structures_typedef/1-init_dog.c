df6ed99bda764affdde9dc5d294f0374388385eb
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-14 10:52:59 +0100

    Fixed task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include <string.h>
+#include <stdlib.h>
 
 /**
 * init_dog - initialize pointer to dog struct
@@ -9,7 +11,20 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
+	if (d == NULL)
+		return;
+	d->name = malloc(strlen(name) + 1);
+	if (d->name == NULL)
+	{
+		return;
+	}
+	strcpy(d->name, name);
 	d->age = age;
-	d->owner = owner;
+	d->owner = malloc(strlen(owner) + 1);
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		return;
+	}
+	strcpy(d->owner, owner);
 }
