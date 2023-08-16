ce609df70361c47b0cbea042c9eae1b8e3170b01
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-17 00:02:15 +0100

    Task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+* int_index - searches an integer in an array
+* @array: array of elements to be searched
+* @size: number of elements in array
+* @cmp: comparator callback function
+*
+* Return: index of array element if found else -1
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	for (; i < size; i++)
+		if (cmp(*(array + i)) != 0)
+			return (i);
+	return (-1);
+}
