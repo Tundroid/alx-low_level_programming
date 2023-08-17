0fe9167f84b4383a148cffee60a98f0eccc8719d
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-17 18:34:15 +0100

    Task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,13 +11,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned long int i = 0;
 	va_list list;
 
-	if (separator != NULL && n > 0)
+	if (n > 0)
 	{
 		va_start(list, n);
 		for (; i < n; i++)
 		{
 			printf("%d", va_arg(list, int));
-			if (i < n - 1)
+			if (separator != NULL && i < n - 1)
 				printf("%s", separator);
 		}
 		va_end(list);
