705ced6814069bed2c9c77c68872a1181c770feb
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-17 02:46:34 +0100

    Task 4fn

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - program Entry point
+* @argc: number of command line args
+* @argv: command line args vector
+*
+* Return: always 0
+*/
+int main(int argc, char **argv)
+{
+	int (*main_ptr)(int, char**);
+	int i = 0, bytes;
+
+	main_ptr = &main;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	unsigned char *opcode = (unsigned char *)main_ptr;
+
+	for (i; i < bytes; i++)
+	{
+		printf("%02x ", opcode[i]);
+	}
+	printf("\b\n");
+	return (0);
+}
