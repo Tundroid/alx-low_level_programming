71e9f7532c8b33659f2b88515a69c6a3c494f031
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-17 19:21:00 +0100

    Fixed task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,35 +11,38 @@ void print_all(const char * const format, ...)
 	va_list list;
 	char *tmp;
 
-	va_start(list, format);
-	while (format[i] != '\0')
+	if (format != NULL)
 	{
-		ignored = 0;
-		switch (format[i])
+		va_start(list, format);
+		while (format[i] != '\0')
 		{
-		case 'c':
-			printf("%c", va_arg(list, int));
-			break;
-		case 'i':
-			printf("%d", va_arg(list, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(list, double));
-			break;
-		case 's':
-			tmp = va_arg(list, char *);
-			if (tmp == NULL)
-				tmp = "(nil)";
-			printf("%s", tmp);
-			break;
-		default:
-			ignored = 1;
-			break;
+			ignored = 0;
+			switch (format[i])
+			{
+			case 'c':
+				printf("%c", va_arg(list, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(list, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(list, double));
+				break;
+			case 's':
+				tmp = va_arg(list, char *);
+				if (tmp == NULL)
+					tmp = "(nil)";
+				printf("%s", tmp);
+				break;
+			default:
+				ignored = 1;
+				break;
+			}
+			if (!ignored && i < (int)strlen(format) - 1)
+				printf(", ");
+			i++;
 		}
-		if (!ignored && i < (int)strlen(format) - 1)
-			printf(", ");
-		i++;
+		va_end(list);
 	}
-	va_end(list);
 	printf("\n");
 }
