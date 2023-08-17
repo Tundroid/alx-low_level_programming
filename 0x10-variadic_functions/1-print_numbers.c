39ba5ac084f74ac2b8fc0c61c56d51d70c8b19af
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-17 18:13:39 +0100

    Task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+* print_numbers - prints varaible params
+* @separator: delimiter
+* @n: variable param count
+* ...: varaible params
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned long int i = 0;
+	va_list list;
+
+	if (separator != NULL)
+	{
+		va_start(list, n);
+		for (; i < n; i++)
+		{
+			printf("%d", va_arg(list, int));
+			if (i < n - 1)
+				printf("%s", separator);
+		}
+		va_end(list);
+	}
+	printf("\n");
+}
