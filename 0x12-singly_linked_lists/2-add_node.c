c99a7d8e569cab51cbba15ab9746f9218270d441
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-24 21:40:54 +0100

    Fixed task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,7 +9,7 @@
 */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *node = malloc(sizeof(list_t));
+	/*list_t *node = malloc(sizeof(list_t));
 
 	if (head == NULL || node == NULL)
 		return (NULL);
@@ -20,5 +20,21 @@ list_t *add_node(list_t **head, const char *str)
 		node->next = *head;
 		*head = node;
 	}
+	return (*head);*/
+	list_t *new;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
+
 	return (*head);
 }
