205d1e4a13550492183d18d49916a3eb0dacb356
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-28 07:05:55 +0100

    Task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,6 +2,7 @@
 #include <string.h>
 
 int wild_helper(int, int, char *, char *);
+int wild_loop(char *, int, int);
 /**
  * wildcmp - string compare using wildcard *
  * @s1: string 1
@@ -28,7 +29,7 @@ int wildcmp(char *s1, char *s2)
  */
 int wild_helper(int s1_len, int s2_len, char *s1, char *s2)
 {
-	int i;
+	int i = 0;
 	int (*wh)(int, int, char *, char *) = wild_helper;
 
 	if (s2_len < 0 && s1_len < 0)
@@ -36,19 +37,27 @@ int wild_helper(int s1_len, int s2_len, char *s1, char *s2)
 	if (s2_len < 0 && s1_len >= 0)
 		return (0);
 	if (s1_len < 0 && s2_len >= 0)
-	{
-		for (i = 0; i <= s2_len; i++)
-		{
-			if (s2[i] != '*')
-				return (0);
-		}
-		return (1);
-	}
+		return (wild_loop(s2, i, s2_len));
 	if (s2[s2_len] == s1[s1_len])
 		return (wh(s1_len - 1, s2_len - 1, s1, s2));
 	if (s2[s2_len] == '*')
-	{
 		return (wh(s1_len, s2_len - 1, s1, s2) || wh(s1_len - 1, s2_len, s1, s2));
-	}
+	return (0);
+}
+
+/**
+* wild_loop - substitute iteration
+* @pattern: string
+* @iterator: pattern cursor
+* @length: pattern length
+*
+* Return: 1 if pattern is a match or 0
+*/
+int wild_loop(char *pattern, int iterator, int length)
+{
+	if (iterator == length)
+		return (1);
+	if (*(pattern + iterator) == '*')
+		return (wild_loop(pattern, iterator++, length));
 	return (0);
 }
