0f2913a52250085a304759c707321efe6bd21b77
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-29 21:49:22 +0100

    Task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,13 +11,13 @@ size_t print_listint_safe(const listint_t *head)
 	listint_t *tmp0 = NULL, *tmp1 = NULL;
 	size_t i, nodes = 0;
 
-	tmp0 = head;
+	tmp0 = (listint_t *)head;
 	while (tmp0 != NULL)
 	{
 		printf("[%p] %d\n", (void *)tmp0, tmp0->n);
 		i = 0;
 		tmp0 = tmp0->next;
-		tmp1 = head;
+		tmp1 = (listint_t *)head;
 		++nodes;
 		while (i < nodes)
 		{
