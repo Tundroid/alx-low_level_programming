7672f977f287bc3485568f45bd70c6ba530cb3ba
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-29 10:58:16 +0100

    Task 11

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+* reverse_listint - inserts node at index of linked list
+* @head: of linked list
+*
+* Return: node at index
+*/
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *tmp, *tmp2;
+
+	if (!head || !*head)
+		return (NULL);
+	if (*head && (*head)->next)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		tmp->next = NULL;
+	}
+	while (*head && (*head)->next)
+	{
+		tmp2 = *head;
+		*head = (*head)->next;
+		tmp2->next = tmp;
+		tmp = tmp2;
+	}
+	(*head)->next = tmp;
+	return (*head);
+}
