784e8451d91e10ada30364b38eaa97e1e39fe78c
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-29 21:45:44 +0100

    Task 12

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+* print_listint_safe - prints a linked list
+* @head: linked list
+*
+* Return: number of nodes
+*/
+size_t print_listint_safe(const listint_t *head)
+{
+	listint_t *tmp0 = NULL, *tmp1 = NULL;
+	size_t i, nodes = 0;
+
+	tmp0 = head;
+	while (tmp0 != NULL)
+	{
+		printf("[%p] %d\n", (void *)tmp0, tmp0->n);
+		i = 0;
+		tmp0 = tmp0->next;
+		tmp1 = head;
+		++nodes;
+		while (i < nodes)
+		{
+			if (tmp0 == tmp1)
+			{
+				printf("-> [%p] %d\n", (void *)tmp0, tmp0->n);
+				return (nodes);
+			}
+			tmp1 = tmp1->next;
+			i++;
+		}
+		if (head == NULL)
+			exit(98);
+	}
+	return (nodes);
+}
