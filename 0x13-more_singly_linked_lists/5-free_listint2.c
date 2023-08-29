171e649da3adfb9b8fd6865cc831d213ee56eade
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-29 06:25:16 +0100

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* free_listint2 - frees a linked list
+* @head: of linked list
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+}
