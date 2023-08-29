60fee1bb1c331b9bf7e2dda2a2e45cab19fc137d
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-29 06:57:01 +0100

    Task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,12 +11,11 @@ int pop_listint(listint_t **head)
 	listint_t *tmp;
 	int n = 0;
 
-	if (head != NULL || *head != NULL)
-	{
-		n = (*head)->n;
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
-	}
+	if (head == NULL || *head == NULL)
+		return (0);
+	n = (*head)->n;
+	tmp = (*head)->next;
+	free(*head);
+	*head = tmp;
 	return (n);
 }
