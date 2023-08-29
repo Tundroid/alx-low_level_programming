4e5012fe6e865192e4c66a96c8f9e94a31bf49f1
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-29 09:28:16 +0100

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,10 +14,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	size_t i = 0;
 
 	node = malloc(sizeof(listint_t));
-	tmp = *head;
-	node->n = n;
 	if (!head || !node)
 		return (NULL);
+	tmp = *head;
+	node->n = n;
 	if (idx == 0)
 	{
 		node->next = *head;
