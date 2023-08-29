a6bc6a8517ed0c0a1648b19acbb32a85409e54be
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-29 09:47:16 +0100

    Task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,6 +18,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index == 0 && *head)
 	{
 		*head = (*head)->next;
+		free(tmp);
 		return (1);
 	}
 	while (tmp)
@@ -25,6 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		if (++i == index)
 		{
 			tmp->next = tmp->next->next;
+			free(tmp->next);
 			return (1);
 		}
 		tmp = tmp->next;
