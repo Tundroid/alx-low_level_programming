ed9f812213228c6ecc86f8e6d556dd0e4a1e17ac
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-31 10:57:26 +0100

    Task 4 fix

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-uint64_t _pow(int, int);
-
 /**
  * clear_bit - converst binary to unsigned int
  * @n: binary string
@@ -12,39 +10,19 @@ uint64_t _pow(int, int);
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	size_t i = 0;
-	unsigned long int m = *n, x = 0;
+	unsigned long int m = *n, x = 0, pow2 = 1;
 	short lsb = 0;
 
 	if (index > 64)
 		return (-1);
-	while (i <= index)
+	while (m)
 	{
 		lsb = m & 1;
 		m >>= 1;
-		if (i++ == index)
-			continue;
-		x |= lsb * _pow(2, i - 1);
+		if (i++ != index)
+			x |= lsb * pow2;
+		pow2 *= 2;
 	}
 	*n &= x;
 	return (1);
-}
-
-/**
-* _pow - raises an integer to the power of another
-* @b: base
-* @p: power
-*
-* Return: b^p
-*/
-uint64_t _pow(int b, int p)
-{
-	if (p == 0)
-		return (1);
-	else if (p == 1)
-		return (b);
-
-	while (p > 0)
-		return (b * _pow(b, --p));
-
-	return (-1);
 }
