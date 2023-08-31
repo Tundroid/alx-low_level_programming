c6f821a2a473ae954e7990e15e2eb7f013d6ab53
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-08-31 10:39:50 +0100

    Task 3 fix

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-uint64_t _pow(int, int);
-
 /**
  * set_bit - converst binary to unsigned int
  * @n: binary string
@@ -11,7 +9,7 @@ uint64_t _pow(int, int);
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	size_t i = 0;
+	size_t i = 0, pow2 = 1;
 
 	if (index > 64)
 		return (-1);
@@ -19,30 +17,11 @@ int set_bit(unsigned long int *n, unsigned int index)
 	{
 		if (i == index)
 		{
-			*n |= _pow(2, i);
+			*n |= pow2;
 			break;
 		}
+		pow2 *= 2;
 		i++;
 	}
 	return (1);
-}
-
-/**
-* _pow - raises an integer to the power of another
-* @b: base
-* @p: power
-*
-* Return: b^p
-*/
-uint64_t _pow(int b, int p)
-{
-	if (p == 0)
-		return (1);
-	else if (p == 1)
-		return (b);
-
-	while (p > 0)
-		return (b * _pow(b, --p));
-
-	return (-1);
 }
