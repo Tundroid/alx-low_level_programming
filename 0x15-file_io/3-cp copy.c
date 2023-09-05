ec6ee8f8fb29eefa894c53050832ed16abd9ac60
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-09-05 03:47:26 +0100

    Task 3

--- 0x15-file_io/3-cp copy.c
+++ 0x15-file_io/3-cp copy.c
@@ -1,16 +1,14 @@
-#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
-char *create_buffer(char *file);
-void close_file(int, char**);
+void close_file(int file);
 
 /**
- * main - Copies the contents of a file to another file.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * main - check the code
+ * @argc: count
+ * @argv: vector
  *
- * Return: 0 on success.
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
@@ -28,30 +26,28 @@ int main(int argc, char *argv[])
 	}
 
 	_open = open(argv[1], O_RDONLY);
+	bzero(buffer, 0);
 	_read = read(_open, buffer, 1024);
 	_open_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
-	do {
-		if (_open == -1 || _read == -1)
-		{
-			dprintf(STDERR_FILENO, err_msgs[1], argv[1]);
-			exit(98);
-		}
-
-		_write_to = write(_open_to, buffer, _read);
+	while (_open != -1 && _read != -1)
+	{
+	    _write_to = write(_open_to, buffer, _read);
 		if (_open_to == -1 || _write_to == -1)
 		{
 			dprintf(STDERR_FILENO, err_msgs[2], argv[2]);
 			exit(99);
 		}
-
+		bzero(buffer, 0);
 		_read = read(_open, buffer, 1024);
 		_open_to = open(argv[2], O_WRONLY | O_APPEND);
-
-	} while (_read > 0);
-
-	close_file(_open, err_msgs);
-	close_file(_open_to, err_msgs);
+	}
+	if (_open < 0 || _read < 0)
+	{
+		dprintf(STDERR_FILENO, err_msgs[1], argv[1]);
+		exit(98);
+	}
+	close_file(_open);
+	close_file(_open_to);
 
 	return (0);
 }
@@ -59,15 +55,14 @@ int main(int argc, char *argv[])
 /**
  * close_file - Closes file descriptors.
  * @file: The file descriptor to be closed.
- * @msgs: error messages
  */
-void close_file(int file, char **msgs)
+void close_file(int file)
 {
 	int c = close(file);
 
 	if (c == -1)
 	{
-		dprintf(STDERR_FILENO, msgs[3], file);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file);
 		exit(100);
 	}
 }
