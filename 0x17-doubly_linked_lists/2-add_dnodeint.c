7f3130d3ba843d7815e020fc5132fe7732b06aca
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-10-12 21:26:56 +0100

    Task 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* add_dnodeint - adds a node to a linked list
+* @head: of linked list
+* @n: integer to add to list
+*
+* Return: address of new node or null on failure
+*/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *node = malloc(sizeof(dlistint_t));
+
+	if (node == NULL)
+		return (NULL);
+	node->n = n;
+	node->next = *head;
+	*head = node;
+	return (*head);
+}
