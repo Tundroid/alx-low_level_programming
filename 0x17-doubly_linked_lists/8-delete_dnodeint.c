6da9daaf361dba23a5c38e37685634cb155bfa9c
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-10-13 01:55:23 +0100

    Task 8

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+* delete_dnodeint_at_index - inserts node at index of linked list
+* @head: of linked list
+* @index: node to be returned
+*
+* Return: node at index
+*/
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp, *del;
+	size_t i = 0;
+
+	if (!head)
+		return (-1);
+	tmp = *head;
+	if (index == 0 && *head)
+	{
+		*head = (*head)->next;
+		if (*head)
+			(*head)->prev = NULL;
+		free(tmp);
+		return (1);
+	}
+	while (tmp)
+	{
+		if (++i == index)
+		{
+			if (!(tmp->next))
+				break;
+			del = tmp->next;
+			tmp->next = tmp->next->next;
+			tmp->next->next->prev = tmp;
+			free(del);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+	return (-1);
+}
