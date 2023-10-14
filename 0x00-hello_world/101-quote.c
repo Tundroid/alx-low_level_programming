1af7bc3361e7523122410f0da17a15cb5d1109f8
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-10-14 14:43:31 +0100

    Revision

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,17 @@
-#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 /**
-* main - Entry point
+* main - prints text to the stderr
 *
-* Return: Always 0 (Success)
+* Return: Always 1 (Error)
 */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\"");
-	fprintf(stderr, " - Dora Korpar, 2015-10-19\n");
+	char *s1 = "and that piece of art is useful\"";
+	char *s2 = " - Dora Korpar, 2015-10-19\n";
+
+	write(STDERR_FILENO, s1, strlen(s1));
+	write(STDERR_FILENO, s2, strlen(s2));
 	return (1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
+* main - prints sizes of some primitive types in bytes
 *
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(long long));
-	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	printf("Size of a char: %hd byte(s)\n", sizeof(char));
+	printf("Size of an int: %hd byte(s)\n", sizeof(int));
+	printf("Size of a long int: %hd byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %hd byte(s)\n", sizeof(long long));
+	printf("Size of a float: %hd byte(s)\n", sizeof(float));
 	return (0);
 }
