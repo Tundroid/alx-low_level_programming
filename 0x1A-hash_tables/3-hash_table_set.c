9d63e3f99f43c468fa45b6834916ecf08b71d8a4
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-11-05 20:03:26 +0100

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,15 +12,37 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new = malloc(sizeof(hash_node_t));
 	size_t index;
+	hash_node_t *tmp;
 
 	if (ht && new && *key)
 	{
+		index = key_index((unsigned char *)key, ht->size);
+		if (ht->array[index])
+		{
+			tmp = ht->array[index];
+			while (tmp)
+			{
+				if (strcmp(tmp->key, key) == 0)
+				{
+					tmp->value = realloc(tmp->value, strlen(value) + 1);
+					if (tmp->value)
+					{
+						strcpy(tmp->value, value);
+						return (1);
+					}
+					else
+					{
+						return (0);
+					}
+				}
+				tmp = tmp->next;
+			}
+		}
 		new->key = strdup(key);
 		new->value = strdup(value);
 		new->next = NULL;
 		if (new->key && new->value)
 		{
-			index = key_index((unsigned char *)key, ht->size);
 			new->next = ht->array[index];
 			ht->array[index] = new;
 			return (1);
