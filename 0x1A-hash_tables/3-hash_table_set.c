679b951176222066608f60b59aa2869894f1a09d
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-11-05 18:04:32 +0100

    Task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - get index of key
+ * @ht: hash table
+ * @key: data key
+ * @value: data value
+ *
+ * Return: 1 if successfully added otherwise 0
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new = malloc(sizeof(hash_node_t));
+	size_t index;
+
+	if (new && *key)
+	{
+		new->key = strdup(key);
+		new->value = strdup(value);
+		new->next = NULL;
+		if (new->key && new->value)
+		{
+			index = key_index((unsigned char *)key, ht->size);
+			new->next = ht->array[index];
+			ht->array[index] = new;
+			return (1);
+		}
+	}
+	return (0);
+}
