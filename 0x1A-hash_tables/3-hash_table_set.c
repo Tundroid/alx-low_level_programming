56a7bbac3d8f867fb21404ec07a254bfbacb14aa
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-11-05 18:23:46 +0100

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new = malloc(sizeof(hash_node_t));
 	size_t index;
 
-	if (new && *key)
+	if (ht && new && *key)
 	{
 		new->key = strdup(key);
 		new->value = strdup(value);
