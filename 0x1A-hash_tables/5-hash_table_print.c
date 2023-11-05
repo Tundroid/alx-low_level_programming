ab1a0c1eb8b9b1b9187262806c08e689fa3c9324
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-11-05 20:50:20 +0100

    Task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	size_t i;
+	hash_node_t *tmp = NULL;
+	bool first = true;
+
+	putchar('{');
+	if (ht)
+	{
+		for (i = 0; i < ht->size; i++)
+		{
+			tmp = ht->array[i];
+			while (tmp)
+			{
+				if (!first)
+					printf(", ");
+				printf("'%s': '%s'", tmp->key, tmp->value);
+				tmp = tmp->next;
+				first = false;
+			}
+		}
+	}
+	printf("}\n");
+}
